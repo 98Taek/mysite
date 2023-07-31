4fbe8a30f9c4315698e7d280aa554e193e606742
Jeong Taek <imjt9345@naver.com>
2023-07-31 16:11:06 +0900

    이미 로그인한 사용자가 로그인 페이지 접근 시 대시보드로 리다이렉트

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from account import views
 
 urlpatterns = [
+    path('login/', views.CustomLoginView.as_view(), name='login'),
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
--- account/views.py
+++ account/views.py
@@ -1,30 +1,19 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from account.forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from account.models import Profile
 
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated Successfully')
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
-    else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+class CustomLoginView(LoginView):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('dashboard')
+        return super().dispatch(request, *args, **kwargs)
 
 
 @login_required
