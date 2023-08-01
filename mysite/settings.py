32cf8edfb88025ecdeb7c2e8934a6579cc173a3e
Jeong Taek <imjt9345@naver.com>
2023-08-01 14:24:26 +0900

    소셜 인증 프로필 추가 로직, 소셜 로그인 파이프라인 수정

--- mysite/settings.py
+++ mysite/settings.py
@@ -165,3 +165,16 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '150881533357-m694qnakbh42f67qdd8al6mijgkb53q4.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
