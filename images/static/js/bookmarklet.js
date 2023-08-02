520dac039e94f21463a87b00827b044e71c0d6bd
Jeong Taek <imjt9345@naver.com>
2023-08-02 13:45:13 +0900

    static 파일 추가, 북마크릿 자바스크립트 코드 작성

---
+++ images/static/js/bookmarklet.js
@@ -0,0 +1,40 @@
+const siteURL = '//127.0.0.1:8000/';
+const styleURL = siteURL + 'static/css/bookmarklet.css';
+const minWidth = 250;
+const minHeight = 250;
+
+var head = document.getElementsByTagName('head')[0];
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = styleURL + '?r=' + Math.floor(Math.random() * 9999999999999999);
+head.appendChild(link);
+
+var body = document.getElementsByTagName('body')[0];
+boxHtml = '<div id="bookmarklet">' +
+    '<a href="#" id="close">&times;</a>' +
+    '<h1>Selet an image to bookmark:</h1>' +
+    '<div class="images"></div>' +
+    '</div>';
+body.innerHTML += boxHtml
+
+function bookmarkletLaunch() {
+    bookmarklet = document.getElementById('bookmarklet');
+    var imagesFound = bookmarklet.querySelector('.images');
+    imagesFound.innerHTML = '';
+    bookmarklet.style.display = 'block';
+    bookmarklet.querySelector('#close').addEventListener('click', function () {
+        bookmarklet.style.display = 'none'
+    });
+    images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
+    images.forEach(image => {
+       if(image.naturalWidth >= minWidth && image.naturalHeight >= minHeight)
+       {
+           var imageFound = document.createElement('img');
+           imageFound.src = image.src;
+           imagesFound.append(imageFound);
+       }
+    });
+}
+
+bookmarkletLaunch();
