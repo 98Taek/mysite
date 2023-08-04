12d9c3c52582f22aae7a05986562b584025bb559
Jeong Taek <imjt9345@naver.com>
2023-08-04 11:44:37 +0900

    액션 select_related, profetch_related 추가

--- account/views.py
+++ account/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.http import require_POST
 
 from account.forms import UserRegistrationForm, UserEditForm, ProfileEditForm
 from account.models import Profile, Contact
+from actions.models import Action
 from actions.utils import create_action
 
 
@@ -20,7 +21,13 @@ class CustomLoginView(LoginView):
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
+    return render(request, 'account/dashboard.html', {'section': 'dashboard', 'actions': actions})
 
 
 def register(request):
