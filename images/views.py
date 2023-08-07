0da2ddca07b5941dca5fa08ff7ca83a5ef75d271
Jeong Taek <imjt9345@naver.com>
2023-08-07 14:35:48 +0900

    redis 코드 주석 처리

--- images/views.py
+++ images/views.py
@@ -11,7 +11,7 @@ from actions.utils import create_action
 from images.forms import ImageCreateForm
 from images.models import Image
 
-r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+# r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
 
 @login_required
@@ -33,8 +33,9 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    total_views = r.incr(f'image: {image.id}:views')
-    r.zincrby('image_ranking', 1, image.id)
+    # total_views = r.incr(f'image: {image.id}:views')
+    # r.zincrby('image_ranking', 1, image.id)
+    total_views = 0
     return render(request, 'images/image/detail.html',
                   {'section': 'images', 'image': image, 'total_views': total_views})
 
@@ -78,10 +79,10 @@ def image_list(request):
     return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
 
 
-@login_required
-def image_ranking(request):
-    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
-    image_ranking_ids = [int(id) for id in image_ranking]
-    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
-    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
-    return render(request, 'images/image/ranking.html', {'section': 'ranking', 'most_viewed': most_viewed})
+# @login_required
+# def image_ranking(request):
+#     image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+#     image_ranking_ids = [int(id) for id in image_ranking]
+#     most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+#     most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+#     return render(request, 'images/image/ranking.html', {'section': 'ranking', 'most_viewed': most_viewed})
