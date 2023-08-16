5c0d9be422a31ce659e3e106d11c53dc3fa6e51f
Jeong Taek <imjt9345@naver.com>
2023-08-16 15:12:43 +0900

    어드민 사이트 상세페이지 이동 링크 추가

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,7 @@ import datetime
 
 from django.contrib import admin
 from django.http import HttpResponse
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 from orders.models import OrderItem, Order
@@ -46,10 +47,15 @@ def order_payment(obj):
 order_payment.short_description = 'Stripe payment'
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', order_payment,
-                    'created', 'updated']
+                    'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
