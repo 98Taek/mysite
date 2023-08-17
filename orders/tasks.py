c8b44366d5d643348f91341acb2ae5f3d742c8b7
Jeong Taek <imjt9345@naver.com>
2023-08-17 10:44:00 +0900

    PDF 이메일 발송

--- orders/tasks.py
+++ orders/tasks.py
@@ -7,7 +7,7 @@ from orders.models import Order
 @shared_task
 def order_created(order_id):
     order = Order.objects.get(id=order_id)
-    subject = f'Order nr. {order.id}'
+    subject = f'Order no. {order.id}'
     message = f'Dear {order.first_name}, \n\n' \
               f'You have successfully placed an order.' \
               f'Your order ID is {order.id}.'
--- payment/webhooks.py
+++ payment/webhooks.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from orders.models import Order
+from .tasks import payment_completed
 
 
 @csrf_exempt
@@ -30,4 +31,5 @@ def stripe_webhook(reqeust):
             order.paid = True
             order.stripe_id = session.payment_intent
             order.save()
+            payment_completed.delay(order.id)
     return HttpResponse(status=200)
